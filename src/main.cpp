a5b8cdde3d0bfaaaf54aacc89f5f44680771c311
gislih24 <gislih24@ru.is>
2026-03-25 09:39:31 +0000

    Change variable names to match Gísli style.

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,11 @@ int main() {
     ::nodelay(::stdscr, TRUE); // Makes getch() non-blocking for concurrency.
 
     // Player coordinates.
-    int p_y = 10;
-    int p_x = 20;
+    int player_y = 10;
+    int player_x = 20;
     // Enemy coordinates.
-    int e_y = 5;
-    int e_x = 0;
+    int enemy_y = 5;
+    int enemy_x = 0;
     const int MAX_ENEMY_DIST = 200;    // Temporary, for the loop.
     const int FRAMES_PER_SECOND = 120; // E.g., 30, 60, 120, ...
     const int REFRESH_INTERVAL_MS = 1000 / FRAMES_PER_SECOND;
@@ -35,27 +35,29 @@ int main() {
         ::mvprintw(0, 0, "Use w,a,s,d to move. Press q to quit.");
 
         attron(COLOR_PAIR(PLAYER_PAIR));
-        ::mvprintw(p_y, p_x, "@"); // Move cursor to (p_y, p_x) and print there.
+        ::mvprintw(player_y, player_x,
+                   "@"); // Move cursor to (player_y, player_x) and print there.
         attroff(COLOR_PAIR(PLAYER_PAIR));
 
         attron(COLOR_PAIR(ENEMY_PAIR));
-        ::mvprintw(e_y, e_x, "#"); // Move cursor to (e_y, e_x) and print there.
+        ::mvprintw(enemy_y, enemy_x,
+                   "#"); // Move cursor to (enemy_y, enemy_x) and print there.
         attroff(COLOR_PAIR(ENEMY_PAIR));
 
         ::refresh(); // Actually pushes the drawing to the screen.
 
         switch (int keyPressed = getch(); keyPressed) {
         case 'w':
-            --p_y;
+            --player_y;
             break;
         case 's':
-            ++p_y;
+            ++player_y;
             break;
         case 'a':
-            --p_x;
+            --player_x;
             break;
         case 'd':
-            ++p_x;
+            ++player_x;
             break;
         case 'q':
             is_running = false;
@@ -65,9 +67,8 @@ int main() {
         }
 
         // Makes the enemy go forward in a loop until it reaches MAX_ENEMY_DIST.
-        e_x = (e_x + 1) % MAX_ENEMY_DIST;
+        enemy_x = (enemy_x + 1) % MAX_ENEMY_DIST;
 
-        // Concurrency, babyyyy!
         std::this_thread::sleep_for(
             std::chrono::milliseconds(REFRESH_INTERVAL_MS));
     }

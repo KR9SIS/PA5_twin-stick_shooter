e680d6b4da3aedc9e271b86aaf46e7e04a0d3e12
Hawkurit <haukurit@gmail.com>
2026-03-25 11:25:06 +0000

    enemy loop wip

--- src/mapstate.cpp
+++ src/mapstate.cpp
@@ -1,22 +1,23 @@
 #include "mapstate.h"
+#include <thread>
+#include <chrono>
 
 MapState::MapState(size_t r, size_t c)
-    : ROWS(r), COLUMNS(c), map(r, std::vector<std::unique_ptr<Entity>>(c)) {}
-    
-
-    // take difficulty as const in map state
-    // instantiate enemies based on difficulty
-    /* 
-    enemy-loop 
-    while (game is running) {
-        for e:enemies {
-            e.act()
-        }
-    this:thread sleep ms
-    }
-    */
+    : difficulty(difficulty), 
+    ROWS(r), 
+    COLUMNS(c), 
+    map(r, std::vector<std::unique_ptr<Entity>>(c)) 
+{
+    game_running = true;
+}
 
 void MapState::run_level(int difficulty) {
     // while (game is running) { for (auto &e : enemies) e.act(); /* sleep */ }
+    while (game_running) {
+        for (auto &enemy : enemies) {
+            enemy.act();
+        }
+    std::this_thread::sleep_for(std::chrono::milliseconds(500))
+    }
 }
 

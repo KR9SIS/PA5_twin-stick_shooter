fe7ce4bba9f71e7fe2d2d0630bec6d2949af76ae
Hawkurit <haukurit@gmail.com>
2026-03-25 11:25:06 +0000

    enemy loop wip

--- include/mapstate.h
+++ include/mapstate.h
@@ -1,4 +1,4 @@
-#include "entities.h"
+#include "entities.hpp"
 #include <memory>
 #include <vector>
 
@@ -9,8 +9,12 @@ class MapState {
     const size_t ROWS;
     const size_t COLUMNS;
     std::vector<std::vector<std::unique_ptr<Entity>>> map;
+    int difficulty;
+    bool game_running;
+    
 
     MapState(size_t rows, size_t columns);
 
     void get_input();
-};
+    std::pair<uint8_t, uint8_t> move_entity(MoveDir direction);
+  };

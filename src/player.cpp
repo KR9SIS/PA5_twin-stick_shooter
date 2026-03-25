1529a6bddb6dce3ff7f0960d72ff31d1ca0a61f3
kr9sis <mkmw464l@anonaddy.com>
2026-03-25 15:38:50 +0000

    Removed unnecessary player functions

--- src/player.cpp
+++ src/player.cpp
@@ -1,16 +1,3 @@
 #include "entities.h"
 
 Player::Player() : Entity(10, 1, 1, 0, 0) {}
-
-void Player::act(Action a) {
-    (void)a;
-}
-
-void Player::attack() {}
-
-int8_t Player::take_damage(int8_t dmg) {
-    cur_hp -= dmg;
-    return cur_hp;
-}
-
-void Player::update() {}

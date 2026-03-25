89348ac9135d7d7e330e62c6dbeec8bdc4995646
Hawkurit <haukurit@gmail.com>
2026-03-25 10:48:21 +0000

    wip

--- src/mapstate.cpp
+++ src/mapstate.cpp
@@ -1,7 +1,20 @@
 #include "mapstate.h"
 
-MapState::MapState(size_t r, size_t c) : ROWS(r), COLUMNS(c), map(r) {
-    for (auto& row : map) {
-        row.resize(c);
+MapState::MapState(size_t r, size_t c)
+    : ROWS(r), COLUMNS(c), map(r, std::vector<std::unique_ptr<Entity>>(c)) {}
+
+// take difficulty as const in map state
+// instantiate enemies based on difficulty
+/*
+enemy-loop
+while (game is running) {
+    for e:enemies {
+        e.act()
     }
+this:thread sleep ms
+}
+*/
+
+void MapState::run_level(int difficulty) {
+    // while (game is running) { for (auto &e : enemies) e.act(); /* sleep */ }
 }

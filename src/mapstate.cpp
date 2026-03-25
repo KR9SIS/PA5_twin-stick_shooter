50b7a8ae3af940863f9d9d3c7afec346af2f6594
Hawkurit <haukurit@gmail.com>
2026-03-25 11:47:18 +0000

    to merge to main

--- src/mapstate.cpp
+++ src/mapstate.cpp
@@ -10,7 +10,7 @@ MapState::MapState(size_t r, size_t c)
 }
 
 void MapState::run_level(int difficulty) {
-    // while (game is running) { for (auto &e : enemies) e.act(); /* sleep */ }
+    std::shared_ptr<MapState> map_p = std::make_shared<MapState>();
     while (game_running) {
         for (auto& enemy : enemies) {
             enemy.act();

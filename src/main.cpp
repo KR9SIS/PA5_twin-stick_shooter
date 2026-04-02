c4e868889fdf748b1e546a84dbac20858572e6a6
kr9sis <mkmw464l@anonaddy.com>
2026-04-02 10:34:17 +0000

    Increased map zize

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@
 int main() {
     const int FRAMES_PER_SECOND = 120;
     NcursesScreen screen(FRAMES_PER_SECOND);
-    MapState map_state(30, 30, 1, screen);
+    MapState map_state(30, 60, 1, screen);
     map_state.run_level();
 
     return 0;
